34a1b5056e4c578fe9393d2042746c988c9616bc
Kazu0210 <clffrdkyl@gmail.com>
2025-03-30 18:32:00 +0800

    "Removed navbar.css and replaced with navbar-backup.css in index.php"

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/index.css">
-    <link rel="stylesheet" href="assets/css/navbar.css">
+    <link rel="stylesheet" href="assets/css/navbar-backup.css">
     <title>E-Commerce | Home</title>
 </head>
 <body>
